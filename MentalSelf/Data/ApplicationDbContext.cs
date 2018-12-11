a7a248100e16f4218b7e51eb338ff3439ae92ae5
davidnicholastaylor <dntaylor14@gmail.com>
2018-12-11 15:58:41 -0600

    finished set up, began seeding database

--- MentalSelf/Data/ApplicationDbContext.cs
+++ MentalSelf/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using MentalSelf.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,5 +13,11 @@ namespace MentalSelf.Data
             : base(options)
         {
         }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<QuestionType> QuestionTypes { get; set; }
+        public DbSet<Response> Responses { get; set; }
+        public DbSet<Test> Tests { get; set; }
+        public DbSet<TestQuestion> TestQuestions { get; set; }
     }
 }
